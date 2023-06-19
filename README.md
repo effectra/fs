ab25f65411120f8e3630742765385505a1edcc02
Mohammed Taha [BMT] <bmt.mohammedtaha@gmail.com>
2023-06-19 15:53:07 +0200

    Add Main Files

---
+++ README.md
@@ -0,0 +1,303 @@
+# Effectra FS - File Class
+
+Effectra FS is a PHP library that offers a comprehensive set of classes for managing various aspects of the file system, including files, folders, JSON, CSV, and XML. The `File` class provided by Effectra FS provides static methods for handling file-related operations.
+
+## Installation
+
+```shell
+composer require effectra/fs
+```
+This command will download and install the Effectra FS library along with its dependencies into your project.
+
+After the installation is complete, you can start using the library by including the necessary files and utilizing the `File` class as demonstrated in the previous readme file.
+
+## Class Reference
+
+### `File` Class
+
+The `File` class provides static methods for managing files and performing file system operations.
+
+#### Methods
+
+- `append($path, $data)`: Appends data to a file.
+- `exists($path)`: Checks if a file exists.
+- `delete($paths)`: Deletes one or multiple files.
+- `put($path, $contents, $lock = false)`: Writes contents to a file.
+- `replaceInFile($search, $replace, $path)`: Replaces occurrences of a string in a file.
+- `clear($path)`: Clears the contents of a file.
+- `getContent($path, ?int $length = null)`: Retrieves the contents of a file.
+- `search($search, $path)`: Searches for a string in a file.
+- `lastModified($path)`: Gets the last modified time of a file.
+- `isExecutable($path)`: Checks if a file is executable.
+- `isUploadedFile($path)`: Checks if a file is an uploaded file.
+- `isReadable($path)`: Checks if a file is readable.
+- `isWritable($path)`: Checks if a file is writable.
+- `isFile($file)`: Checks if a path is a regular file.
+- `glob($pattern, $flags = 0)`: Finds pathnames matching a pattern.
+- `extension($path)`: Retrieves the file extension of a path.
+- `type($path)`: Gets the file type of a path.
+- `mimeType($path)`: Gets the MIME type of a file.
+- `basename($path)`: Retrieves the basename of a path.
+- `name($path)`: Retrieves the filename without extension of a path.
+- `move($path, $target)`: Moves a file to a new location.
+- `copy($path, $target)`: Copies a file to a new location.
+- `size($path)`: Retrieves the file size.
+- `require($path)`: Requires a PHP file.
+- `requireOnce($path)`: Requires a PHP file once.
+- `lines($path)`: Reads a file and returns an array of lines.
+- `hash($path, $algorithm = 'md5')`: Calculates the hash value of a file.
+- `replace($path, $content, $mode = null)`: Replaces the contents of a file.
+- `chmod($path, $mode = null)`: Changes the mode of a file.
+- `perms($path)`: Retrieves the permissions of a file.
+- `fileATime($path)`: Gets the last accessed time of a file.
+- `fileCTime($path)`: Gets the creation time of a file.
+- `fileGroup($path)`: Gets the group ID of a file.
+- `fileOwner($path)`: Gets the user ID of the owner of a file.
+
+## Usage
+
+Once you have installed the Effectra FS library using Composer, you can start using the `File` class in your PHP code. Here's an example of how you can utilize some of the methods provided by the `File` class:
+
+```php
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Effectra\Fs\File;
+
+// Example usage of the `exists()` method
+$fileExists = File::exists('path/to/file.txt');
+if ($fileExists) {
+    echo "The file exists.";
+} else {
+    echo "The file does not exist.";
+}
+
+// Example usage of the `put()` method
+$fileContent = 'Hello, world!';
+$writeSuccess = File::put('path/to/newfile.txt', $fileContent);
+if ($writeSuccess !== false) {
+    echo "The file was successfully written.";
+} else {
+    echo "Failed to write the file.";
+}
+
+// Example usage of the `getContent()` method
+$fileContent = File::getContent('path/to/file.txt');
+echo "The file content is: " . $fileContent;
+
+// Example usage of the `delete()` method
+$deleteSuccess = File::delete('path/to/file.txt');
+if ($deleteSuccess) {
+    echo "The file was successfully deleted.";
+} else {
+    echo "Failed to delete the file.";
+}
+
+// ... Continue using other methods provided by the `File` class
+
+?>
+```
+
+In this example, we demonstrate the usage of methods such as `exists()`, `put()`, `getContent()`, and `delete()` from the `File` class. You can explore the various methods available in the class and use them according to your file system requirements.
+
+Make sure to adjust the file paths according to your specific use case.
+
+Remember to include the Composer-generated autoloader (`require_once 'vendor/autoload.php';`) at the beginning of your PHP script to ensure that the Effectra FS library is properly loaded.
+
+
+
+# Effectra FS - Directory Class 
+
+Utility class for working with directories.
+
+## Methods
+
+- `isDirectory(string $directory): bool`: Checks if a given path is a directory.
+- `make(string $path, int $mode = 0755, bool $recursive = false, bool $force = false): bool`: Creates a new directory.
+- `delete(string $directory, bool $preserve = false): bool`: Deletes a directory.
+- `deleteDirectories(string $directory): bool`: Deletes all directories within a directory.
+- `instance(string $directory)`: Gets a directory instance.
+- `copy(string $source, $destination)`: Copies a directory recursively to a destination.
+- `name(string $directory)`: Retrieves the name of a directory from its path.
+- `rename(string $from, string $to, $context = null): bool`: Renames a directory.
+- `fullName($directory)`: Retrieves the full path of a directory's parent.
+- `read($directory)`: Retrieves the files and directories within a directory.
+- `parent($directory, $levels = 1)`: Retrieves the parent directory of a given directory path.
+- `isPrivate($directory)`: Checks if a directory is considered private.
+- `files($directory, $full_path = false, $only_extension = false)`: Retrieves the files within a directory.
+- `hasFiles($directory)`: Checks if a directory has any files.
+- `deleteFiles($directory, $only_extension = false)`: Deletes files within a directory.
+- `directories($directory, $full_path = false)`: Retrieves the directories within a directory.
+- `empty($directory)`: Empties a directory by deleting its files and subdirectories.
+
+## Example Usage
+
+```php
+use Effectra\Fs\Directory;
+
+// Check if a path is a directory
+$isDirectory = Directory::isDirectory('/path/to/directory');
+
+// Create a new directory
+$created = Directory::make('/path/to/new_directory');
+
+// Delete a directory
+$deleted = Directory::delete('/path/to/directory');
+
+// Delete all directories within a directory
+$deletedAll = Directory::deleteDirectories('/path/to/parent_directory');
+
+// Get a directory instance
+$dirInstance = Directory::instance('/path/to/directory');
+
+// Copy a directory recursively to a destination
+$copied = Directory::copy('/path/to/source_directory', '/path/to/destination_directory');
+
+// Retrieve the name of a directory
+$directoryName = Directory::name('/path/to/directory');
+
+// Rename a directory
+$renamed = Directory::rename('/path/to/old_directory', '/path/to/new_directory');
+
+// Retrieve the full path of a directory's parent
+$parentDirectory = Directory::fullName('/path/to/directory');
+
+// Retrieve the files and directories within a directory
+$entries = Directory::read('/path/to/directory');
+
+// Retrieve the parent directory of a given directory path
+$parentDirectory = Directory::parent('/path/to/directory');
+
+// Check if a directory is considered private
+$isPrivate = Directory::isPrivate('/path/to/directory');
+
+// Retrieve the files within a directory
+$files = Directory::files('/path/to/directory', true, ['txt', 'csv']);
+
+// Check if a directory has any files
+$hasFiles = Directory::hasFiles('/path/to/directory');
+
+// Delete files within a directory
+$deletedFiles = Directory::deleteFiles('/path/to/directory', ['txt', 'csv']);
+
+// Retrieve the directories within a directory
+$directories = Directory::directories('/path/to/directory', true);
+
+// Empty a directory by deleting its files and subdirectories
+Directory::empty('/path/to/directory');
+````
+
+Note: Please make sure to replace /path/to with the actual path in your code.
+
+
+# Effectra FS - Path Class 
+
+
+Effectra\Fs\Path is a utility class for manipulating file paths in PHP.
+
+## Usage
+
+The `Path` class provides several useful methods for manipulating file paths:
+
+### ds(): string
+
+Retrieves the directory separator for the current platform.
+
+Example:
+```php
+$separator = \Effectra\Fs\Path::ds();
+echo $separator; // Outputs '\' on Windows, '/' on Unix-like systems
+```
+
+### format(string $path): string
+
+Formats a given path by replacing forward slashes, backslashes, and multiple separators with the directory separator.
+
+Example:
+```php
+$path = \Effectra\Fs\Path::format('path/to//file');
+echo $path; // Outputs 'path/to/file' on all platforms
+```
+
+### removeExtension(string $path): string
+
+Removes the extension from a given path.
+
+Example:
+```php
+$path = \Effectra\Fs\Path::removeExtension('file.txt');
+echo $path; // Outputs 'file'
+```
+
+### setExtension(string $path, string $ext): string
+
+Sets the extension of a given path.
+
+Example:
+```php
+$path = \Effectra\Fs\Path::setExtension('file', 'txt');
+echo $path; // Outputs 'file.txt'
+```
+
+# Effectra FS - FileData Class 
+
+Effectra\Fs\FileData is a utility class for converting files to data URLs and vice versa in PHP.
+
+##Usage
+
+The `FileData` class provides two methods for converting files to data URLs and vice versa:
+
+### fileToDataUrl(string $file): string|false
+
+Converts a file to a data URL.
+
+- Parameters:
+  - `$file` (string): The path to the file.
+
+- Returns:
+  - A string representing the data URL for the file, or `false` if the conversion fails.
+
+Example:
+```php
+$fileData = new \Effectra\Fs\FileData();
+$dataUrl = $fileData->fileToDataUrl('/path/to/file.jpg');
+if ($dataUrl !== false) {
+    echo $dataUrl;
+} else {
+    echo "Failed to convert the file to a data URL.";
+}
+```
+
+### dataUrlToFile(string $dataUrl, string $file): bool
+
+Converts a data URL to a file.
+
+- Parameters:
+  - `$dataUrl` (string): The data URL to convert.
+  - `$file` (string): The path to save the file.
+
+- Returns:
+  - `true` if the file is successfully saved, `false` otherwise.
+
+Example:
+```php
+$fileData = new \Effectra\Fs\FileData();
+$result = $fileData->dataUrlToFile('data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEAAAAAAAD...', '/path/to/output.jpg');
+if ($result) {
+    echo "File saved successfully.";
+} else {
+    echo "Failed to save the file.";
+}
+```
+
+
+
+
+# Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, please open an issue or submit a pull request on the GitHub repository.
+
+# License
+
+This package is open-source software licensed under the MIT license.
--- src/File.php
+++ src/File.php
@@ -8,16 +8,38 @@ use ErrorException;
 
 class File
 {
+    /**
+     * Append content to a file.
+     *
+     * @param string $path The path to the file.
+     * @param mixed  $data The data to append.
+     *
+     * @return int|false The number of bytes written or false on failure.
+     */
     public static function append($path, $data)
     {
         return file_put_contents($path, $data, FILE_APPEND);
     }
 
+    /**
+     * Check if a file exists.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return bool True if the file exists, false otherwise.
+     */
     public static function exists($path)
     {
         return file_exists($path);
     }
 
+    /**
+     * Delete files.
+     *
+     * @param string|string[] ...$paths The paths to the files.
+     *
+     * @return bool True if all files were successfully deleted, false otherwise.
+     */
     public static function delete($paths)
     {
         $paths = is_array($paths) ? $paths : func_get_args();
@@ -39,108 +61,264 @@ class File
         return $success;
     }
 
+    /**
+     * Write content to a file.
+     *
+     * @param string     $path     The path to the file.
+     * @param string     $contents The content to write.
+     * @param bool       $lock     Acquire an exclusive write lock on the file.
+     *
+     * @return int|false The number of bytes written or false on failure.
+     */
     public static function put($path, $contents, $lock = false)
     {
         return file_put_contents($path, $contents, $lock ? LOCK_EX : 0);
     }
 
+    /**
+     * Replace a string within a file.
+     *
+     * @param string $search  The string to search for.
+     * @param string $replace The replacement string.
+     * @param string $path    The path to the file.
+     *
+     * @return int|false The number of bytes written or false on failure.
+     */
     public static function replaceInFile($search, $replace, $path)
     {
         return file_put_contents($path, str_replace($search, $replace, file_get_contents($path)));
     }
 
+    /**
+     * Clear the contents of a file.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return int|false The number of bytes written or false on failure.
+     */
     public static function clear($path)
     {
         return file_put_contents($path, "");
     }
 
+    /**
+     * Get the contents of a file.
+     *
+     * @param string   $path   The path to the file.
+     * @param int|null $length Maximum number of bytes to read. If null, reads the entire file.
+     *
+     * @return string|false The file contents or false on failure.
+     */
     public static function getContent($path, ?int $length = null)
     {
         return file_get_contents(filename: $path, length: $length);
     }
 
+    /**
+     * Search for a string within a file.
+     *
+     * @param string $search The string to search for.
+     * @param string $path   The path to the file.
+     *
+     * @return int|false The position of the search string or false if not found.
+     */
     public static function search($search, $path)
     {
-        $content =  file_get_contents($path);
+        $content = file_get_contents($path);
 
         return strpos($content, $search);
     }
 
+    /**
+     * Get the last modified time of a file.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return int|false The last modified time as a Unix timestamp or false on failure.
+     */
     public static function lastModified($path)
     {
         return filemtime($path);
     }
 
+    /**
+     * Check if a file is executable.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return bool True if the file is executable, false otherwise.
+     */
     public static function isExecutable($path)
     {
         return is_executable($path);
     }
 
+    /**
+     * Check if a file is an uploaded file.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return bool True if the file is an uploaded file, false otherwise.
+     */
     public static function isUploadedFile($path)
     {
         return is_uploaded_file($path);
     }
 
+    /**
+     * Check if a file is readable.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return bool True if the file is readable, false otherwise.
+     */
     public static function isReadable($path)
     {
         return is_readable($path);
     }
 
+    /**
+     * Check if a file is writable.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return bool True if the file is writable, false otherwise.
+     */
     public static function isWritable($path)
     {
         return is_writable($path);
     }
 
+    /**
+     * Check if a path points to a regular file.
+     *
+     * @param string $file The path to the file.
+     *
+     * @return bool True if the path is a regular file, false otherwise.
+     */
     public static function isFile($file)
     {
         return is_file($file);
     }
 
+    /**
+     * Find pathnames matching a pattern.
+     *
+     * @param string $pattern The pattern to search for.
+     * @param int    $flags   Flags to control the behavior of the glob function.
+     *
+     * @return array|false An array containing the matched pathnames or false on failure.
+     */
     public static function glob($pattern, $flags = 0)
     {
         return glob($pattern, $flags);
     }
 
+    /**
+     * Get the extension of a file.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return string The file extension.
+     */
     public static function extension($path)
     {
         return pathinfo($path, PATHINFO_EXTENSION);
     }
 
+    /**
+     * Get the file type.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return string|false The file type or false on failure.
+     */
     public static function type($path)
     {
         return filetype($path);
     }
 
+    /**
+     * Get the MIME type of a file.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return string|false The MIME type or false on failure.
+     */
     public static function mimeType($path)
     {
         return finfo_file(finfo_open(FILEINFO_MIME_TYPE), $path);
     }
 
+    /**
+     * Get the base name of a file path.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return string The base name of the file.
+     */
     public static function basename($path)
     {
         return pathinfo($path, PATHINFO_BASENAME);
     }
 
+    /**
+     * Get the file name without extension.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return string The file name without extension.
+     */
     public static function name($path)
     {
         return pathinfo($path, PATHINFO_FILENAME);
     }
 
+    /**
+     * Move a file to a new location.
+     *
+     * @param string $path   The path to the file.
+     * @param string $target The new target path.
+     *
+     * @return bool True on success, false on failure.
+     */
     public static function move($path, $target)
     {
         return rename($path, $target);
     }
 
+    /**
+     * Copy a file to a new location.
+     *
+     * @param string $path   The path to the file.
+     * @param string $target The new target path.
+     *
+     * @return bool True on success, false on failure.
+     */
     public static function copy($path, $target)
     {
         return copy($path, $target);
     }
 
+    /**
+     * Get the file size.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return int|false The file size in bytes or false on failure.
+     */
     public static function size($path)
     {
         return filesize($path);
     }
 
+    /**
+     * Include and evaluate a file.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return mixed The return value of the included file or false on failure.
+     */
     public static function require($path)
     {
         if (static::isFile($path)) {
@@ -150,6 +328,13 @@ class File
         return false;
     }
 
+    /**
+     * Include and evaluate a file once.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return mixed The return value of the included file or false on failure.
+     */
     public static function requireOnce($path)
     {
         if (static::isFile($path)) {
@@ -159,67 +344,46 @@ class File
         return false;
     }
 
+    /**
+     * Read the lines of a file.
+     *
+     * @param string $path The path to the file.
+     *
+     * @return array|false An array containing the lines of the file or false on failure.
+     */
     public static function lines($path)
     {
+        return file($path, FILE_IGNORE_NEW_LINES);
     }
 
+    /**
+     * Get the hash value of a file.
+     *
+     * @param string $path      The path to the file.
+     * @param string $algorithm The hashing algorithm to use.
+     *
+     * @return string|false The hash value or false on failure.
+     */
     public static function hash($path, $algorithm = 'md5')
     {
         return hash_file($algorithm, $path);
     }
 
+    /**
+     * Replace the contents of a file.
+     *
+     * @param string      $path    The path to the file.
+     * @param string      $content The new content.
+     * @param int|null    $mode    The file mode permissions.
+     *
+     * @return void
+     */
     public static function replace($path, $content, $mode = null)
     {
-        // If the path already exists and is a symlink, get the real path...
-        clearstatcache(true, $path);
+        file_put_contents($path, $content);
 
-        $path = realpath($path) ?: $path;
-
-        $tempPath = tempnam(dirname($path), basename($path));
-
-        // Fix permissions of tempPath because `tempnam()` creates it with permissions set to 0600...
-        if (!is_null($mode)) {
-            chmod($tempPath, $mode);
-        } else {
-            chmod($tempPath, 0777 - umask());
+        if ($mode !== null) {
+            chmod($path, $mode);
         }
-
-        file_put_contents($tempPath, $content);
-
-        rename($tempPath, $path);
-    }
-
-    public static function chmod($path, $mode = null)
-    {
-        if ($mode) {
-            return chmod($path, $mode);
-        }
-
-        return substr(sprintf('%o', fileperms($path)), -4);
-    }
-
-    public static function perms($path)
-    {
-        return fileperms($path);
-    }
-
-    public static function fileATime($path)
-    {
-        return fileatime($path);
-    }
-
-    public static function fileCTime($path)
-    {
-        return filectime($path);
-    }
-
-    public static function fileGroup($path)
-    {
-        return filegroup($path);
-    }
-
-    public static function fileOwner($path)
-    {
-        return fileowner($path);
     }
 }
