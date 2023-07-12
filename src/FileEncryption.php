3b5c36a3ac60591c6f5b03457b9d84c3d6f75502
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2023-07-12 18:03:46 +0200

    init v2.0.0

--- src/FileEncryption.php
+++ src/FileEncryption.php
@@ -5,17 +5,29 @@ declare(strict_types=1);
 namespace Effectra\Fs;
 
 /**
- * Utility class for manipulating file encryption and decryption
+ * Utility class for file encryption and decryption.
  */
 class FileEncryption
 {
     private string $encryptionKey;
 
+    /**
+     * FileEncryption constructor.
+     *
+     * @param string $encryptionKey The encryption key.
+     */
     public function __construct(string $encryptionKey)
     {
         $this->encryptionKey = $encryptionKey;
     }
 
+    /**
+     * Encrypts a file and saves the encrypted content to a destination file.
+     *
+     * @param string $sourceFile      The path to the source file.
+     * @param string $destinationFile The path to the destination file.
+     * @return void
+     */
     public function encryptFile(string $sourceFile, string $destinationFile): void
     {
         $content = file_get_contents($sourceFile);
@@ -23,6 +35,13 @@ class FileEncryption
         file_put_contents($destinationFile, $encryptedContent);
     }
 
+    /**
+     * Decrypts a file and saves the decrypted content to a destination file.
+     *
+     * @param string $sourceFile      The path to the source file.
+     * @param string $destinationFile The path to the destination file.
+     * @return void
+     */
     public function decryptFile(string $sourceFile, string $destinationFile): void
     {
         $content = file_get_contents($sourceFile);
@@ -30,6 +49,12 @@ class FileEncryption
         file_put_contents($destinationFile, $decryptedContent);
     }
 
+    /**
+     * Encrypts data using AES-256-CBC encryption.
+     *
+     * @param mixed $data The data to encrypt.
+     * @return string The encrypted data.
+     */
     private function encrypt($data): string
     {
         $iv = random_bytes(16);
@@ -37,6 +62,12 @@ class FileEncryption
         return base64_encode($iv . $encryptedData);
     }
 
+    /**
+     * Decrypts data that was encrypted using AES-256-CBC encryption.
+     *
+     * @param string $data The encrypted data.
+     * @return string The decrypted data.
+     */
     private function decrypt($data): string
     {
         $data = base64_decode($data);
